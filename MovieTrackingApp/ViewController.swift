23b7b29cee6e64da3c9f777a8b761707e9bf2ca6
narmin-b <bgvnrm@icloud.com>
2024-12-19 15:43:04 +0400

    Initial Commit

---
+++ MovieTrackingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTrackingApp
+//
+//  Created by Narmin Baghirova on 19.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
